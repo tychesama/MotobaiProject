f0d41da9026a7a8dc7f8bc7347d6c9b0913aa319
tychesama <jridpan1225@gmail.com>
2026-03-09 22:36:16 +0800

    fix: fetch api

--- frontend/src/Components/Hooks/useFetchData.js
+++ frontend/src/Components/Hooks/useFetchData.js
@@ -1,48 +1,27 @@
 import { useState, useEffect } from "react";
-import { ACCESS_TOKEN } from "../../constants";
-
-// needs loading and error handling to pass
+import api from "../../api";
 
 export function useFetchData(info) {
   const [data, setData] = useState([]);
   const [refreshData, setRefreshData] = useState(false);
-  const token = localStorage.getItem(ACCESS_TOKEN);
 
   useEffect(() => {
     const fetchInfo = async () => {
       try {
-        const url = `/api/${info}/list`;
-        const formattedUrl = url.endsWith("/") ? url : `${url}/`;
-
-        const response = await fetch(formattedUrl, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-
-        if (response.status === 401) {
-          console.log("Login first!") // add prompt here that need to login
-          window.location.href = '/login'; 
+        const response = await api.get(`/api/${info}/list/`);
+        const filteredData = response.data.filter((item) => !item.is_deleted);
+        setData(filteredData);
+      } catch (error) {
+        if (error.response?.status === 401) {
+          window.location.href = '/login';
           return;
         }
-
-        if (!response.ok) {
-          throw new Error(`Failed to fetch ${info}s`);
-        }
-
-        const dataThings = await response.json();
-        const filteredData = dataThings.filter((item) => !item.is_deleted);
-        setData(filteredData);
-        console.log(filteredData);
-      } catch (error) {
         console.error(`Error fetching ${info}s:`, error);
       }
     };
 
     fetchInfo();
-  }, [info, token, refreshData]);
+  }, [info, refreshData]);
 
   const triggerRefresh = () => setRefreshData((prev) => !prev);
 
